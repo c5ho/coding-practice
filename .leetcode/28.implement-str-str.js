6a7f4a70b5c07f8f626d5943fddbf447d5df8d19
Calvin Ho <calvin.ch.ho@gmail.com>
2022-08-30 16:05:52 -0400

    Complete Leetcode 28.implement-str-str.js

---
+++ .leetcode/28.implement-str-str.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        return i;
+      }
+    }
+  }
+  return -1;
+};
+
+// @lc code=end
