4d344c35fc5f059cfcd095eadc3146fc117e5f3c
Calvin Ho <calvin.ch.ho@gmail.com>
2022-09-01 18:24:31 -0400

    Complete Leetcode 66.plus-one.js

---
+++ .leetcode/66.plus-one.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=66 lang=javascript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  let last = digits.length - 1;
+  while (digits[last] === 9) {
+    digits[last] = 0;
+    if (last === 0) {
+      digits.splice(0, 0, 1);
+      return digits;
+    }
+    last--;
+  }
+  digits[last]++;
+  return digits;
+};
+// @lc code=end
+
+console.log(plusOne([9, 9, 9]));
