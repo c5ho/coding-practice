4893f81689b7f63462cc6c51cb5b2e0834515aad
Calvin Ho <calvin.ch.ho@gmail.com>
2022-09-01 21:11:13 -0400

    Attempt to solve Leetcode 66.plus-one with strings - incomplete

--- .leetcode/66.plus-one.js
+++ .leetcode/66.plus-one.js
@@ -10,16 +10,26 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  let last = digits.length - 1;
-  while (digits[last] === 9) {
-    digits[last] = 0;
-    if (last === 0) {
-      digits.unshift(1);
-      return digits;
-    }
-    last--;
-  }
-  digits[last]++;
-  return digits;
+  console.log(digits);
+  console.log(typeof digits.join(""));
+  console.log(typeof BigInt(digits.join("").toString(2)));
+  console.log(typeof parseInt(digits.join("").toString(2)));
+  console.log(parseInt(digits.join("").toString(2)));
+  console.log([BigInt(digits.join(""))].toString().split(""));
+  // return [parseInt(digits.join("")) + 1].toString().split("");
 };
 // @lc code=end
+
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+
+// let last = digits.length - 1;
+// while (digits[last] === 9) {
+//   digits[last] = 0;
+//   if (last === 0) {
+//     digits.unshift(1);
+//     return digits;
+//   }
+//   last--;
+// }
+// digits[last]++;
+// return digits;
